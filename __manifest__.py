f0b3078c3ae97f340a821e1044bcdc5903a72ce3
Owais Mirajkar <owais.mirajkar@bista-it-00098.bistasolutions.com>
2025-08-02 10:43:40 +0530

    [ADD] Add changes to manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,36 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "quick_desk",
+    'name': "QuickDesk",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Simple and efficient help desk system for managing support tickets",
 
     'description': """
-Long description of module's purpose
+QuickDesk
+
+The purpose of QuickDesk is to provide a simple, easy-to-use help desk solution
+where users can raise support tickets, and support staff can manage and resolve
+them efficiently. The system aims to streamline communication between users
+and support teams without unnecessary complexity.
     """,
 
     'author': "My Company",
     'website': "https://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Customer Relationship Management',
+    'version': '1.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
+
     'demo': [
         'demo/demo.xml',
     ],
-}
 
+    'application': True,
+    'installable': True,
+}
